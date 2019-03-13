1dc9aace345976326c2bdaa9f7cff4c1e1357259
shrke <shreyamkela@gmail.com>
2019-03-13 15:04:37 -0700

    shwing cards on dashboard

--- frontend/src/containers/HomePage/Dashboard.js
+++ frontend/src/containers/HomePage/Dashboard.js
@@ -32,12 +32,20 @@ class Dashboard extends Component {
   };
 
   render() {
+    let coursesPresent = null;
     console.log("Home Page Reached!");
     //if not logged in go to login page:
     // At the server end, we use res.cookie command of the express-session library, to set the name 'cookie' to the cookie sent to client, when admin logs in. At react/client end, we can check whether the name is 'cookie' or not, to authenticate.
     // At react/client end, we check the cookie name using cookie.load('cookie') command of the 'react-cookies' library. If cookie.load('cookie') != null this means that the user is admin
     // https://stackoverflow.com/questions/44107665/how-to-access-a-browser-cookie-in-a-react-app
-    console.log("COURSES:", this.state.courses[0]);
+    console.log("COURSES:", this.state.courses);
+    if (this.state.courses === "noCourses") {
+      coursesPresent = (
+        <font className="font-weight-bold" size="3">
+          No courses available
+        </font>
+      );
+    }
 
     console.log(cookie.load("cookie"));
     if (!cookie.load("cookie")) {
@@ -51,7 +59,18 @@ class Dashboard extends Component {
           <Layout>
             <SideBar />
           </Layout>
-          <Layout style={{ marginLeft: 150 }}>No courses available</Layout>
+          <Layout style={{ marginLeft: 150 }}>
+            <Header style={{ background: "#fff" }} />
+            <Content
+              style={{
+                background: "#fff",
+                padding: 24,
+                minHeight: 470
+              }}
+            >
+              <div>{coursesPresent}</div> {/**If no courses present */}
+            </Content>
+          </Layout>
         </div>
       );
     }
