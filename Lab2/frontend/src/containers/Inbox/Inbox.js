a54f75df70b8b22c9cc137937f0ada368f8258a7
shreyamkela <shreyamkela@gmail.com>
2019-04-13 22:03:31 -0700

    loading students and faculty of different courses to send messages

--- Lab2/frontend/src/containers/Inbox/Inbox.js
+++ Lab2/frontend/src/containers/Inbox/Inbox.js
@@ -1,45 +1,38 @@
 import React, { Component } from "react";
-
 import { connect } from "react-redux";
 
 import { Layout, List, message, Avatar, Spin } from "antd";
-import reqwest from "reqwest";
 import InfiniteScroll from "react-infinite-scroller";
-
-const fakeDataUrl = "https://randomuser.me/api/?results=5&inc=name,gender,email,nat&noinfo";
 
 class Inbox extends Component {
   state = {
-    data: [],
+    data: "",
     loading: false,
-    hasMore: true
+    hasMore: true,
+    visible: false,
+    validated: false,
+    redirect: false,
+    alert: ""
   };
-  componentDidMount() {
-    this.fetchData(res => {
-      this.setState({
-        data: res.results
-      });
-    });
+
+  async componentDidMount() {
+    const { loginRequest } = this.props; // redux state to props
+    const data = { email: loginRequest.email };
+
+    try {
+      let response = await API.get("messages", { params: data });
+      this.setState({ data: response.data });
+    } catch (error) {
+      console.log(error.response);
+    }
   }
 
-  fetchData = callback => {
-    reqwest({
-      url: fakeDataUrl,
-      type: "json",
-      method: "get",
-      contentType: "application/json",
-      success: res => {
-        callback(res);
-      }
-    });
-  };
-
-  handleInfiniteOnLoad = () => {
-    let data = this.state.data;
+  handleInfiniteOnLoad = async () => {
+    let messages = this.state.data;
     this.setState({
       loading: true
     });
-    if (data.length > 14) {
+    if (messages.length > 14) {
       message.warning("Infinite List loaded all");
       this.setState({
         hasMore: false,
@@ -47,18 +40,71 @@ class Inbox extends Component {
       });
       return;
     }
-    this.fetchData(res => {
-      data = data.concat(res.results);
-      this.setState({
-        data,
-        loading: false
-      });
+    const { loginRequest } = this.props; // redux state to props
+    const data = { email: loginRequest.email };
+    try {
+      let response = await API.get("messages", { params: data });
+      messages = messages.concat(res.results);
+      this.setState({ data: response.data, loading: false });
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  showModal = () => {
+    this.setState({
+      visible: true
     });
+  };
+
+  handleOk = async e => {
+    const { loginRequest } = this.props; // redux state to props
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault(); // dont do default - default is submitting the data to the database
+      e.stopPropagation(); // dont propogate event to parents
+    } else if (this.refs.title.value != "" && this.refs.desc.value != "") {
+      // Only dispatch when both fields are non empty
+      let data = {
+        title: this.refs.title.value,
+        message: this.refs.message.value,
+        email: loginRequest.email,
+        recipent: this.refs.recipent.value
+      };
+      try {
+        let response = await API.post("messages", { data });
+        this.setState({ redirect: true });
+      } catch (error) {
+        console.log(error.response);
+      }
+
+      this.setState({ redirect: true, alert: "Message sent successfully!" });
+    }
+    this.setState({ validated: true });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false
+    });
+  };
+
+  reverseObject = Obj => {
+    // To reverse the allMessages object
+    var TempArr = [];
+    var NewObj = [];
+    for (var Key in Obj) {
+      TempArr.push(Key);
+    }
+    for (var i = 0; i < TempArr.length; i++) {
+      NewObj[TempArr.length - 1 - i] = Obj[i];
+    }
+    return NewObj;
   };
 
   render() {
     const { Header, Content, Footer } = Layout;
-
     return (
       <div>
         {/* FIXME Make the create page a modal */}
@@ -70,6 +116,11 @@ class Inbox extends Component {
               minHeight: 560
             }}
           >
+            <div style={{ textAlign: "right", marginRight: 20 }}>
+              <Button type="primary" shape="round" size="large" icon="plus" onClick={this.showModal}>
+                Message
+              </Button>
+            </div>
             <Layout>
               <div className="demo-infinite-container" style={{ width: 300 }}>
                 <InfiniteScroll
@@ -83,8 +134,13 @@ class Inbox extends Component {
                     dataSource={this.state.data}
                     renderItem={item => (
                       <List.Item key={item.id}>
-                        <List.Item.Meta title={<a href="https://ant.design">{item.name.last}</a>} description={item.email} />
-                        <div>Content</div>
+                        <List.Item.Meta
+                          title={
+                            <a href="#" onClick={this.handleTitleClick}>
+                              {item.title}
+                            </a>
+                          }
+                        />
                       </List.Item>
                     )}
                   >
@@ -100,6 +156,20 @@ class Inbox extends Component {
           </Content>
           <Footer style={{ background: "#fff" }} />
         </Layout>
+        <Modal title="Send a message:" visible={this.state.visible} onOk={e => this.handleOk(e)} onCancel={this.handleCancel}>
+          <Form noValidate validated={validated}>
+            {/* FIXME Fix the size of the text boxes. Also, description feild should be a text area */}
+            <Form.Group as={Col} controlId="validationTitle">
+              <Form.Label>Title</Form.Label>
+              <Form.Control required type="text" placeholder="Enter Title" ref="title" />
+            </Form.Group>
+            <Form.Group as={Col} controlId="validationMessage">
+              <Form.Label>Message</Form.Label>
+              <Form.Control required as="textarea" rows="3" placeholder="Enter Message" ref="message" />
+            </Form.Group>
+          </Form>
+          <div className="text-success">{this.state.alert}</div>
+        </Modal>
       </div>
     );
   }
