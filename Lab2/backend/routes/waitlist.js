c9698f8918e96eea820facefe5f5091a33a723dc
shreyamkela <shreyamkela@gmail.com>
2019-04-14 16:12:33 -0700

    updating document insert

--- Lab2/backend/routes/waitlist.js
+++ Lab2/backend/routes/waitlist.js
@@ -7,57 +7,41 @@ router.post("/", function(req, res) {
   console.log("Waitlisting into a course!");
   let waitlistData = req.body.data;
   console.log("Waitlist data: ", waitlistData);
-
-  let alreadyEnrolled = false;
-
   let alreadyWaitlisted = false;
   if (waitlistData.courseId != undefined) {
     // First we check if waitlistData.courseId is undefined
     // Then we check whether this course is already waitlisted
     // Then we check whether this course is already enrolled
     // Then we insert
-    db.query(`SELECT * FROM coursewaitlists WHERE CourseId = '${waitlistData.courseId}'`, (err, results) => {
-      if (err) throw err;
-      console.log(results);
-      for (var key in results) {
-        if (results[key].StudentEmail == waitlistData.email) {
-          console.log("Course already waitlisted for this email!");
-          alreadyWaitlisted = true;
-          res.status(400).end("Course already waitlisted!"); // res.end will end the response here and dont go futher in this post request? But this doesnt work here why? return res.end also doesnt work if a db.query is after this db.query
-          break;
+
+    Model.userDetails.findOne(
+      {
+        email: waitlistData.email
+      },
+      (err, user) => {
+        if (err) {
+          console.log("Unable to fetch user", err);
+        } else {
+          if (user) {
+            console.log("Course already waitlisted for this email!");
+            alreadyWaitlisted = true;
+            res.status(400).end("Course already waitlisted!"); // res.end will end the response here and dont go futher in this post request? But this doesnt work here why? return res.end also doesnt work if a db.query is after this db.query
+          } else {
+            user.waitlistedCourses.push(waitlistData.course);
+            user.save().then(
+              doc => {
+                console.log("New details added to this user details", doc);
+                res.send("Addition Successful!");
+              },
+              err => {
+                console.log("Unable to save course details.", err);
+                res.status(400).send();
+              }
+            );
+          }
         }
       }
-      if (alreadyWaitlisted !== true) {
-        db.query(`SELECT * FROM courseenrolments WHERE CourseId = '${waitlistData.courseId}'`, (err, results_enrolled) => {
-          if (err) throw err;
-          for (var key in results_enrolled) {
-            if (results_enrolled[key].StudentEmail == waitlistData.email) {
-              // If already enrolled then throw error
-              console.log("Cannot waitlist as course is already enrolled for this email!");
-              alreadyEnrolled = true;
-              res.status(400).end("Cannot waitlist as course is already enrolled!"); // res.end will end the response here and dont go futher in this post request? But this doesnt work here why? return res.end also doesnt work if a db.query is after this db.query
-              break;
-            }
-          }
-          if (alreadyEnrolled !== true) {
-            db.query(`INSERT INTO coursewaitlists (CourseId, StudentEmail) VALUES ('${waitlistData.courseId}','${waitlistData.email}')`, err => {
-              if (err) throw err;
-              console.log("New details added to coursewaitlists table");
-            });
-            db.query(`SELECT WaitlistUsed FROM courses WHERE Id= '${waitlistData.courseId}'`, (err, results_courses) => {
-              if (err) throw err;
-              // Increment the WaitlistUsed
-              let waitlistUsed = results_courses[0].WaitlistUsed + 1;
-              db.query(`UPDATE courses SET WaitlistUsed = '${waitlistUsed}' WHERE Id = '${waitlistData.courseId}'`, err => {
-                if (err) throw err;
-                console.log("WaitlistUsed updated in the courses table");
-                res.status(200).end("Course waitlisted!");
-              });
-            });
-          }
-        });
-      }
-    });
+    );
   }
 });
 
--- Lab2/backend/uploads/_helpers/insertDocuments.js
+++ Lab2/backend/uploads/_helpers/insertDocuments.js
@@ -17,15 +17,34 @@ const upload = multer({ storage: storage });
 module.exports = insertDocuments = function(Model, file, courseId, email, callback) {
   console.log("Insert file link called!");
 
-  db.query(
-    `INSERT INTO Files (Email, CourseId, Path, Folder) VALUES ('${email}', '${courseId}', '../../uploads/${file.folder}/${file.filePath}', '${
-      file.folder
-    }'  )`,
-    err => {
-      if (err) throw err;
-      console.log("New details added to Files table");
-      res.send("Addition Successful!");
-      callback(result);
+  Model.courseDetails.findOne(
+    {
+      courseId: courseId
+    },
+    (err, user) => {
+      if (err) {
+        console.log("Unable to fetch course", err);
+      } else {
+        if (user) {
+          user.files.email.push(email);
+          user.files.courseId.push(courseId);
+          user.files.path.push(`../${file.folder}/${file.filePath}`);
+          user.files.folder.push(announcementData);
+          user.save().then(
+            doc => {
+              console.log("New details added to this course files", doc);
+              res.send("addition Successful!");
+              callback(user);
+            },
+            err => {
+              console.log("Unable to save file details.", err);
+              res.status(400).send();
+            }
+          );
+        } else {
+          res.status(400).send();
+        }
+      }
     }
   );
 };
