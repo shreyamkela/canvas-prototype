8d75c5c3998a4e1bdbb317f821cc7335d3a58127
shreyamkela <shreyamkela@gmail.com>
2019-04-14 21:02:08 -0700

    kafka services - updating the profile picture

--- Lab2/backend/routes/permissionnumber.js
+++ Lab2/backend/routes/permissionnumber.js
@@ -1,29 +1,23 @@
 //Route to handle Get Request Call show all students that are on the waitlist and require permission numbers, for a particular course
 const express = require("express");
 const router = express.Router();
-const Model = require("../database/connection");
+var kafka = require("../kafka/client");
 
 router.get("/", function(req, res) {
   console.log("Get waitlist/permission number data!");
   // ANCHOR
-  let waitlistData = req.query; // In GET request, req.query is used to access the data sent from frontend in params
-  Model.courseDetails.findOne(
-    {
-      courseId: waitlistData.courseId
-    },
-    (err, user) => {
-      if (err) {
-        console.log("Unable to fetch course", err);
+  kafka.make_request("permissionnumber", req, function(err, result) {
+    if (err) {
+      console.log("Unable to fetch course", err);
+    } else {
+      if (result) {
+        console.log("Waitlist detail: ", result);
+        res.status(200).send(result.waitlistedStudents);
       } else {
-        if (user) {
-          console.log("Waitlist detail: ", user);
-          res.status(200).send(user.waitlistedStudents);
-        } else {
-          res.status(400).send();
-        }
+        res.status(400).send();
       }
     }
-  );
+  });
 });
 
 //Route to handle Post Request Call to provide permission number to select students that are on the waitlist, for a particular course
@@ -31,36 +25,17 @@ router.get("/", function(req, res) {
 router.post("/", function(req, res) {
   console.log("Post permission number data!");
   // ANCHOR
-  let waitlistData = req.body.data;
-  // let title = waitlistData.title;
-  // console.log("Title: ", title);
-
-  Model.courseDetails.findOne(
-    {
-      courseId: waitlistData.courseId
-    },
-    (err, user) => {
-      if (err) {
-        console.log("Unable to fetch course", err);
+  kafka.make_request("permissionnumber", req, function(err, result) {
+    if (err) {
+      console.log("Unable to fetch course", err);
+    } else {
+      if (result) {
+        res.send("Addition Successful!");
       } else {
-        if (user) {
-          user.waitlistedStudents.push(waitlistData.permissionnumber);
-          user.save().then(
-            doc => {
-              console.log("New details added to this course waitlist", doc);
-              res.send("Addition Successful!");
-            },
-            err => {
-              console.log("Unable to save waitlist details.", err);
-              res.status(400).send();
-            }
-          );
-        } else {
-          res.status(400).send();
-        }
+        res.status(400).send();
       }
     }
-  );
+  });
 });
 
 module.exports = router;
--- Lab2/backend/routes/profile.js
+++ Lab2/backend/routes/profile.js
@@ -1,70 +1,39 @@
 //Route to handle Get Request Call to load all details of profile
 const express = require("express");
 const router = express.Router();
-const Model = require("../database/connection");
+var kafka = require("../kafka/client");
 
 router.get("/", function(req, res) {
   console.log("Get Profile Data Called!");
-  let profileData = req.query; // In GET request, req.query is used to access the data sent from frontend in params
-
-  Model.userDetails.findOne(
-    {
-      email: profileData.email
-    },
-    (err, user) => {
-      if (err) {
-        console.log("Unable to fetch user", err);
+  kafka.make_request("profile", req, function(err, result) {
+    if (err) {
+      console.log("Unable to fetch user", err);
+    } else {
+      if (result) {
+        console.log("User details: ", result);
+        res.status(200).send(result);
       } else {
-        if (user) {
-          console.log("User details: ", user);
-          res.status(200).send(user);
-        } else {
-          res.status(400).send();
-        }
+        res.status(400).send();
       }
     }
-  );
+  });
 });
 
 router.post("/", function(req, res) {
   console.log("Edit Profile Data Posted!");
   let profileData = req.body.data;
   console.log(profileData);
-
-  Model.userDetails.findOne(
-    {
-      email: profileData.email
-    },
-    (err, user) => {
-      if (err) {
-        console.log("Unable to fetch user", err);
+  kafka.make_request("profile", req, function(err, result) {
+    if (err) {
+      console.log("Unable to fetch user", err);
+    } else {
+      if (user) {
+        res.send("Addition Successful!");
       } else {
-        if (user) {
-          user.aboutMe.push(profileData.profile.aboutMe);
-          user.gender.push(profileData.profile.gender);
-          user.contactNumber.push(profileData.profile.contactNumber);
-          user.city.push(profileData.profile.city);
-          user.country.push(profileData.profile.country);
-          user.company.push(profileData.profile.company);
-          user.school.push(profileData.profile.school);
-          user.hometown.push(profileData.profile.hometown);
-          user.languages.push(profileData.profile.languages);
-          user.save().then(
-            doc => {
-              console.log("New details added to this user profile", doc);
-              res.send("Addition Successful!");
-            },
-            err => {
-              console.log("Unable to save profile details.", err);
-              res.status(400).send();
-            }
-          );
-        } else {
-          res.status(400).send();
-        }
+        res.status(400).send();
       }
     }
-  );
+  });
 });
 
 module.exports = router;
