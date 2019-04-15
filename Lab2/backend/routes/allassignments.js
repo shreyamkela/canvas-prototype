9e811c7fea4ea6cede183f15c255b1ae8afafdf3
shreyamkela <shreyamkela@gmail.com>
2019-04-14 18:30:31 -0700

    kafka services - all assignments

--- Lab2/backend/routes/allassignments.js
+++ Lab2/backend/routes/allassignments.js
@@ -1,32 +1,22 @@
 //Route to handle Get Request Call to show assignments created in a particular course. If persona is student, show whether an assignment is submitted or not. If the persona is faculty, show all the assignments created.
 const express = require("express");
 const router = express.Router();
-const Model = require("../database/connection");
+var kafka = require("../kafka/client");
 
 router.get("/", function(req, res) {
   console.log("Get All Assignments Data Called!");
   // ANCHOR
-  let assignmentData = req.query; // In GET request, req.query is used to access the data sent from frontend in params
-
-  Model.courseDetails.findOne(
-    {
-      courseId: assignmentData.courseId
-    },
-    (err, user) => {
-      if (err) {
-        console.log("Unable to fetch assignments detail.", err);
-      } else {
-        if (user) {
-          console.log("Assignments detail: ", user);
-          for (key in user) {
-            res.status(200).sendFile(__dirname + `${user[key].Path}`);
-          }
-        } else {
-          res.status(400).send();
-        }
+  kafka.make_request("allassignments", req, function(err, result) {
+    if (err) {
+      console.log("Error in fetching all assignments.", err);
+      res.status(400).send();
+    } else {
+      console.log("Assignments detail: ", result);
+      for (key in result) {
+        res.status(200).sendFile(__dirname + `${result[key].Path}`);
       }
     }
-  );
+  });
 });
 
 module.exports = router;
